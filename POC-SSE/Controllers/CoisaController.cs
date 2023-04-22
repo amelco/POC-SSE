36afca7aaed0a57dc23fa5863f6e4f868b54cda5
Andre Herman <amelco.herman@gmail.com>
2023-04-21 21:07:52 -0300

    refactor: renomeia classes

---
+++ POC-SSE/Controllers/CoisaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POC_SSE.Models;
+
+namespace POC_SSE.Controllers
+{
+
+    [ApiController]
+    [Route("/coisas")]
+    public class CoisaController : ControllerBase
+    {
+
+        private List<CoisaModel> casos = new List<CoisaModel>
+        {
+            new CoisaModel
+            {
+                Id = Guid.NewGuid(),
+                Codigo = "0001B",
+                Nome = "Fulano de Tal"
+            },
+            new CoisaModel
+            {
+                Id = Guid.NewGuid(),
+                Codigo = "0002A",
+                Nome = "Beltrano de Tal"
+            }
+        };
+
+        [HttpGet]
+        public IActionResult ListarCoisas(CancellationToken cancellationToken)
+        {
+            Task.Delay(500);
+            return Ok(casos);
+        }
+    }
+}
---
+++ POC-SSE/Models/CoisaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace POC_SSE.Models
+{
+    public class CoisaModel
+    {
+        public Guid Id { get; set; }
+        public string? Codigo { get; set; }
+        public string? Nome { get; set; }
+
+    }
+}
